b3017fd42d00419e4ea79c63f8d88243473d2c9d
Ali Razghandi <a.razghandi11@gmail.com>
2021-08-09 16:52:19 +0430

    write basic methods for the blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 from time import time
 
 
@@ -16,6 +18,9 @@ class Blockchain:
             'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """add a new transaction to mempool"""
@@ -24,10 +29,31 @@ class Blockchain:
 
     @staticmethod
     def hash(block):
-        """hash a block"""
-        pass
+        """return sha256 hash of a block"""
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         """return the last block of the blockchain"""
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """check the proof and return a boolean as a result"""
+        guess_proof = f"{proof}{last_proof}".encode()
+        guess_proof_hash = hashlib.sha256(guess_proof).hexdigest()
+        return guess_proof_hash[:4] == '0000'
+
+    def proof_of_work(self, last_proof):
+        """"
+        Simple Proof of Work Algorithm:
+        Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        p is the previous proof, and p' is the new proof
+        """
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
