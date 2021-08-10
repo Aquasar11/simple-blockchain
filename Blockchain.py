aaa20ec8a4120e4ea957ece1c62af77f383e3244
Ali Razghandi <a.razghandi11@gmail.com>
2021-08-10 18:55:18 +0430

    add consensus methods

--- Blockchain.py
+++ Blockchain.py
@@ -1,8 +1,10 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
 from uuid import uuid4
 
+import requests as requests
 from flask import Flask, jsonify, request
 
 
@@ -11,6 +13,53 @@ class Blockchain:
         self.chain = []
         self.current_transactions = []
         self.new_block(11, "first block in the blockchain created by Aquasar")
+        self.nodes = set()
+
+    def register_node(self, address):
+        """register a node into blockchain"""
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """check a given chain and validate it"""
+        current_index = 0
+        last_block = chain[current_index]
+        current_index += 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """check all blockchain nodes and determine the chosen chain"""
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            res = requests.get(f'http://{node}/chain')
+
+            if res.status_code == 200:
+                length = res.json()['length']
+                chain = res.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
     def new_block(self, proof, previous_hash=None):
         """create a new block"""
@@ -73,8 +122,8 @@ def mine():
     previous_block = blockchain.last_block
     previous_proof = previous_block['proof']
 
-    new_proof = blockchain.proof_of_work(previous_proof)
     blockchain.new_transaction('protocol', 'Ali', 100)
+    new_proof = blockchain.proof_of_work(previous_proof)
 
     previous_hash = blockchain.hash(previous_block)
     new_block = blockchain.new_block(new_proof, previous_hash)
@@ -111,6 +160,39 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
     }
+    return jsonify(res), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_node():
+    value = request.get_json()
+
+    nodes = value.get('nodes')
+    for node in nodes:
+        blockchain.register_node(node)
+
+    res = {
+        'message': "nodes added",
+        'total nodes': list(blockchain.nodes)
+    }
+
+    return jsonify(res), 201
+
+
+@app.route('/consensus')
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        res = {
+            'message': "chain replaced",
+            'new chain': blockchain.chain
+        }
+    else:
+        res = {
+            'message': "chain didn't replaced in this machine",
+            'new chain': blockchain.chain
+        }
+
     return jsonify(res), 200
 
 
